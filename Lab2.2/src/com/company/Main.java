b5d396414fb647e4fae762fc29f166552b5edd80
Sadchikov-ikbo-06-20 <79518170+Bronepodrostok@users.noreply.github.com>
2021-10-14 10:14:26 +0300

    Add files via upload

---
+++ Lab2.2/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    MyString str = new MyString("челик");
+        System.out.println(str.compareTo(new MyString("микро челик")));
+    }
+}
