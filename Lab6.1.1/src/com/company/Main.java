1600da63a4a9e45c5c99e564fe594a20bcc89067
Sadchikov-ikbo-06-20 <79518170+Bronepodrostok@users.noreply.github.com>
2021-12-16 10:03:42 +0300

    Fix

--- Lab6.1.1/src/com/company/Main.java
+++ Lab6.1.1/src/com/company/Main.java
@@ -1,42 +1,39 @@
 package com.company;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
-
-
     public static Boolean compare(int a, int b){
-        if(a==0&&b==9) return true;
+        if ((a==0 && b==9)) return true;
+        if ((a==9 && b==0)) return false;
         if(a>b) return true;
         return false;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Print first deck:");
-        Queue<Integer> st1 = new LinkedList<Integer>();
+        Scanner cin = new Scanner(System.in);
+        System.out.print("Print first deck: ");
+        Deque<Integer> q1 = new ArrayDeque<>();
         int tmp;
         for (int i = 0; i < 5; i++) {
-            tmp = scanner.nextInt();
-            st1.add(tmp);
+            tmp = cin.nextInt();
+            q1.add(tmp);
         }
-        System.out.println("Print second deck:");
-        Queue<Integer> st2 = new LinkedList<Integer>();
+        System.out.print("Print second deck: ");
+        Deque<Integer> q2 = new ArrayDeque<>();
         for (int i = 0; i < 5; i++) {
-            tmp = scanner.nextInt();
-            st2.add(tmp);
+            tmp = cin.nextInt();
+            q2.add(tmp);
         }
         int count =0;
-        while (!st1.isEmpty()&&!st2.isEmpty()){
-            System.out.println(st1);
-            System.out.println(st2);
-            if (compare(st1.peek(),st2.peek())) {
-                st1.add(st2.poll());
+        while (!q1.isEmpty()&&!q2.isEmpty()){
+            System.out.println(count + ": " + q1 + "\n   " + q2);
+            if (compare(q1.peek(),q2.peek())) {
+                q1.addLast(q1.poll());
+                q1.add(q2.poll());
             } else {
-                st2.add(st1.poll());
+                q2.add(q1.poll());
+                q2.addLast(q2.poll());
             }
             count++;
             if(count>105){
@@ -45,8 +42,7 @@ public class Main {
             }
         }
 
-        if(st1.isEmpty()) System.out.println("First "+count);
+        if(q2.isEmpty()) System.out.println("First "+count);
         else System.out.println("Second "+count);
-
     }
 }
